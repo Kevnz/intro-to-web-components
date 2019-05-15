95bc1fadc8de189cf0ba7d53b9f642a18512456d
Kevin Isom <kevin.isom@gmail.com>
2019-05-15 13:50:40 +1200

    Create a theme

--- theme.js
+++ theme.js
@@ -1,1 +1,111 @@
-export { yellow as theme } from 'mdx-deck/themes'
+
+import React from 'react'
+
+const yellow = '#FFD500'
+const baseTheme = {
+  font: 'system-ui, sans-serif',
+  monospace: 'Menlo, monospace',
+
+  colors: {
+    text: '#000',
+    background: 'white',
+    link: '#07c',
+    pre: '#f0f',
+    preBackground: '#333',
+    code: '#f0f',
+  },
+  css: {
+    fontSize: '16px',
+    textAlign: 'center',
+    '@media screen and (min-width:56em)': {
+      fontSize: '32px',
+    },
+    '@media screen and (min-width:64em)': {
+      fontSize: '48px',
+    },
+    '@media print': {
+      fontSize: '40px',
+    },
+    'li > p': {
+      margin: 0,
+    },
+  },
+  pre: {
+    textAlign: 'left',
+  },
+  ol: {
+    textAlign: 'left',
+  },
+  ul: {
+    textAlign: 'left',
+  },
+}
+const theme = {
+  ...baseTheme,
+  font: '"Roboto Condensed", system-ui, sans-serif',
+  weights: [400, 700],
+  monospace: '"Roboto Mono", monospace',
+  googleFont:
+    'https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700|Roboto+Mono',
+  colors: {
+    text: '#0D1117',
+    background: yellow,
+    link: '#333',
+    pre: yellow,
+    preBackground: '#0D1117',
+    code: yellow,
+    codeBackground: '#0D1117',
+  },
+  heading: {
+    fontWeight: 'normal',
+    fontSize: 72,
+
+
+  },
+  blockquote: {
+    fontWeight: 700,
+  },
+  pre: {
+    textAlign: 'left',
+  },
+  root: {
+    border: '30px solid green',
+  },
+  Slide: {
+    // borderLeft: '1px solid #0D1117',
+    '& > div': {
+
+    },
+  },
+  css: {
+    '#root': {
+      border: '20px solid black',
+    }
+  }
+}
+const Provider = props => (
+  <div>
+    {props.children}
+    <div
+      css={{
+        position: 'fixed',
+        right: 0,
+        bottom: 0,
+        margin: 16,
+      }}
+    >
+      <a href="https://kevinisom.info" css={{
+        color: '#0D1117',
+        textDecoration: 'none',
+        '&:hover': {
+          color: '#0D1117',
+        }
+      }}>Kevin Isom</a>
+    </div>
+  </div>
+)
+
+export default {
+  Provider,
+  ...theme
+}
