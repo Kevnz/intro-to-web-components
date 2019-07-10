7cea2828fdb9ca455f4ea38d51ef9c1485018599
Kevin Isom <kevin.isom@gmail.com>
2019-07-11 08:20:06 +1200

    example links

--- publish.js
+++ publish.js
@@ -1,4 +1,9 @@
+const fs = require('fs')
 const ghpages = require('gh-pages')
+
+fs.copyFileSync('./samples/element-details.html', './dist/samples/element-details.html')
+fs.copyFileSync('./samples/hello.html', './dist/samples/hello.html')
+fs.copyFileSync('./samples/popup.html', './dist/samples/popup.html')
 
 ghpages.publish(
   'dist',
